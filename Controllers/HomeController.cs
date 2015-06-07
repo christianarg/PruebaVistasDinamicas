4a54fcb3978bc7cce2ecbaf559f5c0914e2a4969
christianarg <christianarg@gmail.com>
2015-06-07 20:11:42 +0200

    First Commit :)
    (Tengo que borrar toda la basura que instala la plantilla de mvc)

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaVistasDinamicas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            return View();
+        }
+
+        public ActionResult Jose()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ShowEditForm()
+        {
+            return View(InMemoryStorage.templateCode);
+        }
+
+        public ActionResult ResetTemplate()
+        {
+            InMemoryStorage.SetDefaultTemplate();
+            return RedirectToAction("Index");
+        }
+
+        [ValidateInput(false)]
+        public ActionResult EditView(TemplateCodeModel templateCode)
+        {
+            InMemoryStorage.templateCode.Code = templateCode.Code;
+            return RedirectToAction("Index");
+        }
+    }
+
+    public static class InMemoryStorage
+    {
+        public const string defaultTemplate = "<h1>Hola Mostro</h1>";
+
+        public static TemplateCodeModel templateCode;
+
+        public static void SetDefaultTemplate()
+        {
+            templateCode.Code = defaultTemplate;
+        }
+
+        static InMemoryStorage()
+        {
+            templateCode = new TemplateCodeModel();
+            SetDefaultTemplate();
+        }
+    }
+
+    public class TemplateCodeModel
+    {
+        public string Code { get; set; }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,68 @@
+﻿using PruebaVistasDinamicas.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace PruebaVistasDinamicas
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CustomRazorViewEngine());
+
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+        }
+    }
+
+    public class CustomRazorViewEngine : RazorViewEngine
+    {
+        protected override bool FileExists(ControllerContext controllerContext, string virtualPath)
+        {
+            if (IsDynamicView(virtualPath))
+            {
+                CreateViewFiler(virtualPath);
+            }
+            return base.FileExists(controllerContext, virtualPath);
+        }
+
+        private static void CreateViewFiler(string virtualPath)
+        {
+            var viewName = virtualPath.Split('/')[1];
+
+
+            string viewHtml = InMemoryStorage.templateCode.Code;
+            var viewPath = HostingEnvironment.MapPath(virtualPath);
+
+            //MemoryCache.Default.get
+            // OJO: Asumimos que existe la carpeta /DynamicViews/
+
+            File.Delete(viewPath);
+            File.AppendAllText(path: viewPath,
+                contents: viewHtml);
+        }
+
+        private static bool IsDynamicView(string virtualPath)
+        {
+            return virtualPath.ToLower().Contains("/dynamicviews/");
+        }
+    }
+}
